3feada2d1029a0f92dac77605e2ff4ed55f16765
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-05-04 05:59:35 +0500

    Add files via upload

---
+++ Conditional Statement/Conditional Statement/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conditional Statement");
+
+
+Console.WriteLine("If else");
+start:
+
+Console.WriteLine("Enter Your Username");
+string username = Console.ReadLine();
+Console.WriteLine("Enter Your Password");
+int password = int.Parse(Console.ReadLine());
+
+if (username == "sarfaraz" && password == 1234)
+{
+    Console.WriteLine("Login Successfull");
+}
+else
+{
+    Console.WriteLine("Login Failed!");
+    goto start;
+}
